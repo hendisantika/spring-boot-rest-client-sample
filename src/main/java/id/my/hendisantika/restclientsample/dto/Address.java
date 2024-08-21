d4291c4f493fb452579071dd5d630f6d81fcde93
hendisantika <hendisantika@yahoo.co.id>
2024-08-22 06:45:44 +0700

    Add Address DTO 🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/restclientsample/dto/Address.java
@@ -0,0 +1,28 @@
+package id.my.hendisantika.restclientsample.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-rest-client-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 22/08/24
+ * Time: 06.43
+ * To change this template use File | Settings | File Templates.
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Address {
+    private String street;
+    private String suite;
+    private String city;
+    private String zipcode;
+    private Geo geo;
+}
