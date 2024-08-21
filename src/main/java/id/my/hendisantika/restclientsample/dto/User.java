8127bc7535b12699847f3705559fa6d7330da040
hendisantika <hendisantika@yahoo.co.id>
2024-08-22 06:46:03 +0700

    Add User DTO 🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/restclientsample/dto/User.java
@@ -0,0 +1,31 @@
+package id.my.hendisantika.restclientsample.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-rest-client-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 22/08/24
+ * Time: 06.42
+ * To change this template use File | Settings | File Templates.
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class User {
+    private long id;
+    private String name;
+    private String username;
+    private String email;
+    private Address address;
+    private String phone;
+    private String website;
+    private Company company;
+}
