cb600c313277b3c3671da3795d252d23f3412448
hendisantika <hendisantika@yahoo.co.id>
2024-08-22 06:35:01 +0700

    Create Spring BOot Project 🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/restclientsample/SpringBootRestClientSampleApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.restclientsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestClientSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRestClientSampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/restclientsample/SpringBootRestClientSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.restclientsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestClientSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
